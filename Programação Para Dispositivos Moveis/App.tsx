ac55bc98431889898954965af38c6fd2a852c146
Adrian Nery <125079978+Nerydeveloper@users.noreply.github.com>
2024-08-21 08:50:59 -0300

    Add files via upload

---
+++ Programação Para Dispositivos Moveis/App.tsx
@@ -0,0 +1,9 @@
+import IndexScreen from "./src/features";
+
+
+export default function App() {
+  return (
+    <IndexScreen/>
+  );
+}
+
---
+++ Programação Para Dispositivos Moveis/src/features/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import styles from "./global/styles";
+
+function IndexScreen() {
+    return(
+        <View style={styles.container}>
+            
+            <Text>Naynime</Text>
+        </View>
+    )
+}
+
+export default IndexScreen;
